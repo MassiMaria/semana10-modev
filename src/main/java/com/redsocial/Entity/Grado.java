ce86370fb7afe2c28be91fca9e2c81a551d38070
massi <massi@LAPTOP-URITQ3TT>
2020-11-21 10:40:14 -0500

    Mi primer commit

---
+++ src/main/java/com/redsocial/Entity/Grado.java
@@ -0,0 +1,41 @@
+package com.redsocial.Entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "grado")
+public class Grado {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int idGrado;
+	private String nombre;
+	
+	
+	
+	public int getIdGrado() {
+		return idGrado;
+	}
+	public void setIdGrado(int idGrado) {
+		this.idGrado = idGrado;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+}
+
+
+
